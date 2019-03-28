7107f850ac2ac460a75ad4cf84bae40dbc14f969
GreedoRen <greedo.it@gmail.com>
2019-03-28 22:37:35 +0300

    Add bold and italic text

---
+++ src/components/TextEditor.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import {Editor} from 'slate-react';
+import {Value} from 'slate';
+import {BoldMark, ItalicMark} from './index';
+
+
+const initialValue = Value.fromJSON({
+    document: {
+        nodes: [
+            {
+                object: 'block',
+                type: 'paragraph',
+                nodes: [
+                    {
+                    object: 'text',
+                    leaves: [
+                        {
+                            text: 'My first paragraph!',
+                        },
+                    ],
+
+                    },
+                ],
+            },
+        ],
+    },
+})
+
+export default class TextEditor extends Component {
+
+onKeyDown = (e, change) => {
+    if(!e.ctrlKey) {return}
+    e.preventDefault()
+
+    switch(e.key) {
+        case 'b': {
+            change.toggleMark('bold')
+            return true
+        }
+        case 'i' : {
+            change.toggleMark('italic')
+            return true
+        }
+        default: {
+            return;
+        }
+    }
+}
+
+
+renderMark = props => {
+    // eslint-disable-next-line default-case
+    switch(props.mark.type) {
+        case 'bold':
+            return <BoldMark {...props} />
+
+        case 'italic':
+            return <ItalicMark {...props} />
+    }
+}
+
+
+
+
+    state = {
+        value: initialValue
+    }
+
+    onChange = ({value}) => {
+        this.setState({value})
+    }
+
+    render() {
+        return (
+            <Editor 
+                value={this.state.value}
+                onChange={this.onChange} 
+                onKeyDown={this.onKeyDown} 
+                renderMark={this.renderMark}
+                />
+        )
+    }
+}
